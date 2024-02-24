afa0bacf168f3ea46cd44c77f6470991a705eae6
btkcodedev <btk.codedev@gmail.com>
2024-02-24 14:55:04 +0530

    Deploy gh-pages and firebase

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: "/favicon.ico",
-          destination: "/favicon/favicon.ico", // Serve the files from the root
-        },
-        {
-          source: "/apple-touch-icon.png",
-          destination: "/apple-touch-icon/", // Serve the files from the root
-        },
-        {
-          source: "/android-chrome/:path*",
-          destination: "/android-chrome/:path", // Serve the files from the root
-        },
-      ];
-    },
+  output: "export",
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/favicon.ico",
+  //       destination: "/favicon/favicon.ico", // Serve the files from the root
+  //     },
+  //     {
+  //       source: "/apple-touch-icon.png",
+  //       destination: "/apple-touch-icon/", // Serve the files from the root
+  //     },
+  //     {
+  //       source: "/android-chrome/:path*",
+  //       destination: "/android-chrome/:path", // Serve the files from the root
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
